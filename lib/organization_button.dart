1bc8bb7c60e7f49bd038b622e17c77b468e5c378
PercyChang18 <79681586+PercyChang18@users.noreply.github.com>
2021-12-12 21:06:03 -0800

    Update organization_button.dart
    add comment

--- lib/organization_button.dart
+++ lib/organization_button.dart
@@ -2,15 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 
-///
+///@author Kuang-Chi Chang
+///@version 11/18/2021
+
+///Represents indinidual organization setting button
 class OrganizationButton extends StatelessWidget {
   final String buttonTitle;
   final Color colour;
 
-  ///
+  ///Constructs the button
   const OrganizationButton(this.buttonTitle, this.colour);
 
-  ///
+  ///Builds the button
   @override
   Widget build(BuildContext context) {
     return Container(
