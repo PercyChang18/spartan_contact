947c744d9493daf2c2cd108ee67e0005d552ab79
PercyChang18 <79681586+PercyChang18@users.noreply.github.com>
2021-12-12 21:02:32 -0800

    Update constants.dart
    add comment

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 /// TODO: fill this out
-/// @author
-/// @version
+/// @author Kuang-Chi Chang
+/// @version 11/18/2021
 const kButtonSelectedColor = Color(0xFF2DC4EF);
 const kButtonNotSelectedColor = Color(0xFF3b95fa);
